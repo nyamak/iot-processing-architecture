59a744f2466b907f11c10b03077018d6d30e4eaa
Nicolas <nicolas.yamakoshi@gmail.com>
2022-10-05 01:22:50 +0200

    chore: Remove premature AWS provider.

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -1,25 +1,16 @@
 terraform {
   required_providers {
-    aws = {
-        source = "hashicorp/aws"
-        version = ">= 4.31.0"
-    }
     kubernetes = {
-        source = "hashicorp/kubernetes"
-        version = ">= 2.13.1"
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.13.1"
     }
   }
 
   required_version = ">= 0.14"
 }
 
-provider "aws" {
-    profile = "default"
-    region = "eu-west-1"
-}
-
 provider "kubernetes" {
-    config_path = "~/.kube/config"
+  config_path = "~/.kube/config"
 }
 
 provider "helm" {
