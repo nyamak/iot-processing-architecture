9006758100f1e7108e48b0647be2b4e6318d50a4
Nicolas <nicolas.yamakoshi@gmail.com>
2023-05-30 16:41:44 -0300

    infra: Enable logging and persistence.

--- infrastructure/broker.tf
+++ infrastructure/broker.tf
@@ -7,4 +7,16 @@ resource "helm_release" "mosquitto" {
     name  = "service.ports.mqtt"
     value = var.mqtt_port
   }
+  set {
+    name = "mosquitto.config"
+    value = <<EOT
+allow_anonymous true
+log_type all
+EOT
+  }
+
+  set {
+    name = "mosquitto.persistence.enabled"
+    value = true
+  }
 }
