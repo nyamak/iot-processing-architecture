87d666415f156ab2c7e8cd2f6ee52ee7f59882a1
Nicolas <nicolas.yamakoshi@gmail.com>
2023-06-01 17:58:49 -0300

    chore: Add new variabels to config.

--- microservices/notifications/src/config.py
+++ microservices/notifications/src/config.py
@@ -6,10 +6,12 @@ if os.environ.get("ENV") == "DEV":
     load_dotenv()
 
 config = {
-    "PORT": int(os.environ.get("PORT", 5000)),
     "SENDGRID_API_KEY": os.environ.get("SENDGRID_API_KEY"),
     "SENDGRID_FROM_EMAIL": os.environ.get("SENDGRID_FROM_EMAIL"),
     "SENDGRID_TO_EMAIL": os.environ.get("SENDGRID_TO_EMAIL"),
-    "REDIS_HOST": os.environ.get("REDIS_HOST"),
-    "REDIS_PORT": os.environ.get("REDIS_PORT"),
+    "PORT": int(os.environ.get("PORT", 5000)),
+    "REDIS_HOST": os.environ.get("REDIS_HOST", "localhost"),
+    "REDIS_PORT": int(os.environ.get("REDIS_PORT", 6379)),
+    "ENV": os.environ.get("ENV"),
+    "EMAIL_COOLDOWN": int(os.environ.get("EMAIL_COOLDOWN", 60)),
 }
